8974cbc9df21f2051cf5721d3af8df340752357e
vaelentine <magdalenegeise@gmail.com>
2021-06-23 10:52:46 -0700

    update/cleanup formatting

--- mags/SWNUtilities/GameApp/urls.py
+++ mags/SWNUtilities/GameApp/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ModelsDropDown, index, CharacterList, CharacterDelete, CharacterUpdate, CharacterCreate, CharacterDetail, PartyCreate, PartyUpdate, PartyCharacters
-
 
 urlpatterns = [
-    path('', index.index, name='index'),
+    path('', views.index.index, name='index'),
     # path('CharacterCreation/', views.CharacterCreation, name='character_creation'),
     path('models/',
-         ModelsDropDown.models_choice,
+         views.ModelsDropDown.models_choice,
          name='models'),
 
     path('character/list/',
@@ -15,32 +13,29 @@ urlpatterns = [
          name='character_list'),
 
     path('character/create/',
-         CharacterCreate.as_view(),
+         views.CharacterCreate.as_view(),
          name='character_create'),
 
     path('character/<int:pk>/detail/',
-         CharacterDetail.as_view(),
+         views.CharacterDetail.as_view(),
          name='character_detail'),
 
     path('character/<int:pk>/update/',
-         CharacterUpdate.as_view(),
+         views.CharacterUpdate.as_view(),
          name='character_update'),
 
     path('character/<int:pk>/delete/',
-         CharacterDelete.as_view(),
+         views.CharacterDelete.as_view(),
          name='character_delete'),
 
     path('party/create/',
-         PartyCreate.as_view(),
+         views.PartyCreate.as_view(),
          name='party_create'),
 
     path('party/<int:pk>/update/',
-         PartyUpdate.as_view(),
+         views.PartyUpdate.as_view(),
          name='party_update'),
 
     path('party/list/',
-         PartyCharacters.as_view()),
-
-    # path('party/add/',
-    #      )
+         views.PartyCharacters.as_view()),
 ]
