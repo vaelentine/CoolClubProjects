1da217a5bc8c3fd3538d0e5be263cacc24671674
vaelentine <magdalenegeise@gmail.com>
2021-06-24 14:02:20 -0700

    update party model to display name

--- mags/SWNUtilities/GameApp/models/Party.py
+++ mags/SWNUtilities/GameApp/models/Party.py
@@ -4,3 +4,6 @@ from django.db import models
 class Party(models.Model):
     name = models.CharField(max_length=100, null=True)
     # TODO: location = models.ForeignKey('Location', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.name)
