51d31bce54e1edeab03ee47c2ea5732e6c04f5d8
vaelentine <magdalenegeise@gmail.com>
2021-06-24 14:29:30 -0700

    fix procedure violation; undo changes from last two commits

--- mags/SWNUtilities/GameApp/models/Party.py
+++ mags/SWNUtilities/GameApp/models/Party.py
@@ -4,6 +4,3 @@ from django.db import models
 class Party(models.Model):
     name = models.CharField(max_length=100, null=True)
     # TODO: location = models.ForeignKey('Location', on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return str(self.name)
