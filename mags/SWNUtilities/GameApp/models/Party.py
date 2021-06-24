5a9c208aeb44561ba783eb84dd4adac596612a5e
vaelentine <magdalenegeise@gmail.com>
2021-06-24 14:52:27 -0700

    default Party model object ref to name; enforce unique

--- mags/SWNUtilities/GameApp/models/Party.py
+++ mags/SWNUtilities/GameApp/models/Party.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class Party(models.Model):
-    name = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=True, unique=True)
     # TODO: location = models.ForeignKey('Location', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
