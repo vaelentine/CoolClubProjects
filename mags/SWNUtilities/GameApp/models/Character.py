9c1d9dd55455672e433a31a78591fd5bb2ec0e5d
vaelentine <magdalenegeise@gmail.com>
2021-06-27 22:10:24 -0700

    update character model

--- mags/SWNUtilities/GameApp/models/Character.py
+++ mags/SWNUtilities/GameApp/models/Character.py
@@ -4,7 +4,7 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=100, unique=True)
     player = models.ForeignKey('Player', related_name='character_players', null=True, blank=True, on_delete=models.SET_NULL)
-    attributes = models.ForeignKey('Attributes', null=True, blank=True, related_name='character_attributes', on_delete=models.SET_NULL)
+    attributes = models.OneToOneField('Attributes', null=True, blank=True, related_name='character_attributes', on_delete=models.SET_NULL)
     faction = models.ForeignKey('Faction', null=True, blank=True, related_name='character_factions', on_delete=models.SET_NULL)
     party = models.ForeignKey('Party', null=True, blank=True, related_name='character_party', on_delete=models.SET_NULL)
     pronouns = models.ForeignKey('Pronouns', null=True, blank=True, related_name='character_pronouns', on_delete=models.SET_NULL)
