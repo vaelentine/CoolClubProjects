849d0c8f66dce84d318c6cf733123bc8264268b2
vaelentine <magdalenegeise@gmail.com>
2021-07-06 16:07:25 -0700

    update character model

--- mags/SWNUtilities/GameApp/models/Character.py
+++ mags/SWNUtilities/GameApp/models/Character.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Character(models.Model):
     name = models.CharField(
         max_length=100,
         unique=True,
         null=True)
+
     player = models.ForeignKey(
         'Player',
         related_name='character_players',
@@ -13,6 +15,7 @@ class Character(models.Model):
         blank=True,
         on_delete=models.SET_NULL
     )
+
     attributes = models.OneToOneField(
         'Attributes',
         null=True,
@@ -20,6 +23,7 @@ class Character(models.Model):
         related_name='character_attributes',
         on_delete=models.CASCADE
     )
+
     faction = models.ForeignKey(
         'Faction',
         null=True,
@@ -27,12 +31,15 @@ class Character(models.Model):
         related_name='character_factions',
         on_delete=models.SET_NULL
     )
+
     party = models.ForeignKey(
         'Party',
         null=True,
         blank=True,
         related_name='character_party',
-        on_delete=models.SET_NULL)
+        on_delete=models.SET_NULL
+    )
+
     pronouns = models.ForeignKey(
         'Pronouns',
         null=True,
@@ -40,6 +47,7 @@ class Character(models.Model):
         related_name='character_pronouns',
         on_delete=models.SET_NULL
     )
+
     background = models.ForeignKey(
         'Background',
         null=True,
@@ -47,13 +55,60 @@ class Character(models.Model):
         related_name='character_backgrounds',
         on_delete=models.SET_NULL
     )
-    # character_class = models.ForeignKey(
-    #     'CharacterClass',
+
+    skills = models.ManyToManyField(
+        'Skill',
+        through='SkillLevel',
+        related_name='character_skills',
+    )
+
+    character_class = models.ForeignKey(
+        'CharacterClass',
+        null=True,
+        blank=True,
+        related_name='character_classes',
+        on_delete=models.SET_NULL
+    )
+
+    # home_world = models.ForeignKey(
+    #     'World',
+    #     related_name='home_world',
+    #     on_delete=models.SET_NULL,
     #     null=True,
     #     blank=True,
-    #     related_name='character_classes',
-    #     on_delete=models.SET_NULL
     # )
+    character_level = models.PositiveSmallIntegerField(
+        default=1
+    )
+
+    max_hit_points = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True
+    )
+
+    current_hit_points = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True
+    )
+
+    foci = models.ManyToManyField(
+        'Focus',
+        through='FocusLevel',
+        related_name='character_foci',
+    )
+
+    psionic_disciplines = models.ManyToManyField(
+        'PsionicDiscipline',
+        through='PsionicDisciplineLevel',
+    )
+
+    psionic_abilities = models.ManyToManyField(
+        'PsionicTechnique',
+    )
+
+    psychic_effort = models.PositiveSmallIntegerField(
+        default=0
+    )
 
     def __str__(self):
         return str(self.name)
