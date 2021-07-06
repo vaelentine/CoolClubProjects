5147cefe152e9ee8a02f088719aae7281d8e0cd4
vaelentine <magdalenegeise@gmail.com>
2021-07-06 16:07:02 -0700

    update imports

--- mags/SWNUtilities/GameApp/models/__init__.py
+++ mags/SWNUtilities/GameApp/models/__init__.py
@@ -6,4 +6,8 @@ from .Faction import Faction
 from .Party import Party
 from .Pronouns import Pronouns
 from .Background import Background
-from .Skill import Skill
+from .Skill import Skill, SkillLevel
+from .Focus import Focus, FocusLevel, FocusAbility
+from .PsionicDiscipline import PsionicDiscipline, PsionicDisciplineLevel
+from .PsionicTechnique import PsionicTechnique
+from .CharacterClass import CharacterClass
